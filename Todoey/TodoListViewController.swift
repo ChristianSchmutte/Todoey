d64697c14835e31733656d0fc3bff5cd1d1d911e
Christian Schmutte <christianschmutte@gmail.com>
2019-04-12 12:39:13 +0200

    Initial Commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Christian Schmutte on 12.04.19.
+//  Copyright © 2019 Christian Schmutte. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController{
+
+    let itemsArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: Challenge - Table View Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: Challenge 2 - TableViewDelegateMethod: selected row does something
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
